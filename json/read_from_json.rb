9b5535d35110701011979636dcff88359eeb6622
Garen Hart <garenhart@gmail.com>
2022-05-24 20:03:59 -0400

    Fix file path

--- json/read_from_json.rb
+++ json/read_from_json.rb
@@ -1,5 +1,5 @@
 # require 'json' # works without this, so commented out
-file = File.read(get(:sp_path)+'test\json\sample_data.json')
+file = File.read(get(:sp_path)+'SonicPi_tests\json\sample_data.json')
 data_hash = JSON.parse(file)
 puts data_hash.keys[2]
 puts data_hash['books']
